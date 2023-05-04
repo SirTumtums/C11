27eb793f4f2592b0f8df32001ec707cbba8ff9bd
Blake Vongsavath <buddhasak.vongsavath@gmail.com>
2023-05-03 23:16:48 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3001
+const htmlRoutes = require('./routes/htmlR')
+const apiRoutes = require('./routes/apiR')
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.use('/api', apiRoutes)
+app.use('/', htmlRoutes)
+
+app.listen(PORT, () =>
+console.log(`App listening at http://localhost${PORT}`))
